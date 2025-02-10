714e15e2043b8a78ab02e74916bad5383986ef9d
Aratimius <aratimius@gmail.com>
2025-02-10 17:32:52 +0300

    admin panel settings

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order, Dish
+
+
+@admin.register(Order)
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display = ('table_number', 'status', 'total_price',)
+
+
+@admin.register(Dish)
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price',)
