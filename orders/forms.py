243a397f9896e6b0e3004044393f92c64e3fcdfe
Aratimius <aratimius@gmail.com>
2025-02-11 20:53:38 +0300

    CRUD operations for Order have been created

---
+++ orders/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm, BooleanField
+from orders.models import Order
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class OrderCreateForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Order
+        fields = ('table_number', 'items',)
+
+
+class OrderUpdateForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Order
+        fields = ('status',)
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,13 @@
-from django.views.generic import ListView
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from orders.forms import OrderCreateForm, OrderUpdateForm
 from orders.models import Order
 
 
 class OrderListView(ListView):
-    """Эндпоинт для отображения списка заказов"""
+    """Отображение списка заказов"""
 
     model = Order
 
@@ -19,3 +22,60 @@ class OrderListView(ListView):
         context_data['proceeds'] = proceeds
 
         return context_data
+
+    def get_queryset(self):
+        """Реализация поиска заказа"""
+        queryset = super().get_queryset()
+
+        # ПОИСК ПО НОМЕРУ СТОЛА ИЛИ ПО СТАТУСУ ЗАКАЗА:
+        search = self.request.GET.get("search", "")
+        if search.isdigit():
+            queryset = queryset.filter(Q(table_number=search))
+            return queryset
+        elif search:
+            # ВНИМАНИЕ! Если поиск идет по статусу заказа, то вводить статус необходимо так:
+            # WAITING, PAID или READY
+            queryset = queryset.filter(Q(status=search))
+            return queryset
+        else:
+            return queryset
+
+
+class OrderDetailView(DetailView):
+    """Детальный просмотр заказа"""
+
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        """Вынести в контекст блюда из заказа в отдельную переменную"""
+
+        context_data = super().get_context_data(**kwargs)
+        dishes = [item.name for item in Order.objects.get(pk=self.kwargs['pk']).items.all()]
+        context_data['dishes'] = dishes
+
+        return context_data
+
+
+class OrderCreateView(CreateView):
+    """Создание заказа"""
+
+    model = Order
+    form_class = OrderCreateForm
+    success_url = reverse_lazy('orders:list')
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    form_class = OrderUpdateForm
+    success_url = reverse_lazy('orders:list')
+
+
+class OrderDeleteView(DeleteView):
+    model = Order
+    success_url = reverse_lazy('orders:list')
+
+
+
+
+
+
