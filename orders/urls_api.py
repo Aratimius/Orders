e96f2cc5f42e0e34d094243a175582b5f2c4feda
Aratimius <aratimius@gmail.com>
2025-02-13 17:59:40 +0300

    Ass API documentation

--- orders/urls_api.py
+++ orders/urls_api.py
@@ -1,13 +1,32 @@
-from rest_framework import routers
+from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 from orders.apps import OrdersConfig
 from orders.views_api import OrderViewSet
-
 
 app_name = OrdersConfig.name
 
 router = routers.SimpleRouter()
 router.register("", OrderViewSet)
 
-urlpatterns = []
+#  ДОБАВЛЕНИЕ ДОКУМЕНТАЦИИ
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
 
 urlpatterns += router.urls
